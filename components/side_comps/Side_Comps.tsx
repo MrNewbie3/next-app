fe23fcdc09c5686fa16b1294c77b97c1b61d0e6a
MrNewbie3 <atharafiaffandy@gmail.com>
2023-03-19 13:49:21 +0700

    dev: removing console

--- components/side_comps/Side_Comps.tsx
+++ components/side_comps/Side_Comps.tsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
 import React, { Component } from "react";
+import { MdSportsSoccer } from "react-icons/md";
 
 async function getData() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos?_start=10&_limit=5");
   if (!res.ok) {
     throw new Error("Failed to fetch data = " + res.statusText);
   }
@@ -11,14 +12,18 @@ async function getData() {
 
 export default async function Side_Comps() {
   const data = await getData();
-  console.log(data);
   return (
     <>
       <div className="flex flex-col gap-y-4 ">
         {data.map((value: any) => {
           return (
-            <Link href={`/${value.userId}`} key={value.id}>
-              <button className="bg-primary-red text-white w-full px-5 py-3 rounded-lg ">{value.title}</button>
+            <Link href={`/${value.id}`} key={value.id}>
+              <button className="bg-primary-red text-white w-full px-5 py-3 rounded-lg flex flex-row items-center gap-x-3 ">
+                <div className="icon">
+                  <MdSportsSoccer className="text-xl" />
+                </div>
+                <p>{value.title}</p>
+              </button>
             </Link>
           );
         })}
