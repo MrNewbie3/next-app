9b2a2ba6b90609abe21a4a5c4e7602401fe69508
lakyulakyu <martieszhidan@gmail.com>
2023-04-18 21:29:48 +0700

    created chart js

--- components/matchRekap.tsx
+++ components/matchRekap.tsx
@@ -1,5 +1,9 @@
+"use client";
 import React from "react";
 import { IoMdArrowDropdown } from "react-icons/io";
+import { useEffect, useRef } from "react";
+import Chart from "chart.js/auto";
+
 
 // untuk memasukkan data pada range===!!
 const maxtackle = 15;
@@ -14,6 +18,76 @@ const swon = (value50_50s / max50_50s) * 100;
 const maxiwon = 7;
 const valueiwon = 2;
 const iwon = (valueiwon / maxiwon) * 100;
+
+//untuk memasukkan data pada  passing & ball position
+const SuccesfulPass=2
+const UnsuccelfulPass=  10
+const HasilPass=Math.floor((SuccesfulPass/UnsuccelfulPass)*100);
+
+const SuccesfulFinal=4
+const UnsuccelfulFinal=7
+const HasilFinal= Math.floor((SuccesfulFinal/UnsuccelfulFinal)*100);
+
+
+const ChartPosition = () => {
+  const chartRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    if (chartRef.current) {
+      const ctx = chartRef.current.getContext("2d");
+      if (ctx) {
+        new Chart(ctx, {
+          type: "doughnut",
+          data: {
+            datasets: [
+              {
+                data: [SuccesfulPass, UnsuccelfulPass],
+                backgroundColor: ["#EFE616", "#137403"],
+                hoverBackgroundColor: ["#EFE616", "#137403"],
+                borderWidth: 10,
+              },
+            ],
+          },
+          options: {
+            spacing: 1,
+          },
+        });
+      }
+    }
+  }, []);
+
+  return <canvas ref={chartRef} />;
+};
+
+const ChartPasses = () => {
+  const chartRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    if (chartRef.current) {
+      const ctx = chartRef.current.getContext("2d");
+      if (ctx) {
+        new Chart(ctx, {
+          type: "doughnut",
+          data: {
+            datasets: [
+              {
+                data: [78, 10],
+                backgroundColor: ["#D00D00", "#2846AF"],
+                hoverBackgroundColor: ["#D00D00", "#2846AF"],
+                borderWidth: 10,
+              },
+            ],
+          },
+          options: {
+            spacing: 1,
+          },
+        });
+      }
+    }
+  }, []);
+
+  return <canvas ref={chartRef} />;
+};
 
 function MatchRekap() {
   return (
@@ -54,67 +128,63 @@ function MatchRekap() {
         </div>
 
         <div className="max-w-4xl w-full  bg-white  rounded-lg px-6 py-4 ">
-          <div className="mb-6 text-[20px] font-semibold">Passing & Ball Position </div>
-          <div className="flex  max-lg:flex-col lg:gap-20">
+          <div className="mb-6 text-[20px] font-semibold">
+            Passing & Ball Position{" "}
+          </div>
+          <div className="flex  max-lg:flex-col lg:gap-20 gap-0">
             <div className="lg:max-w-sm w-full">
               <div className="flex justify-between mb-4">
                 <div className="  flex gap-2 items-center ">
                   <div className="inset-0 bg-red-500 h-2 w-2 rounded-full"></div>
                   <h1 className="font-semibold">Succesful pases</h1>
                 </div>
-                <p className="font-bold text-base">25</p>
+                <p className="font-bold text-base">{SuccesfulPass}</p>
               </div>
               <div className="flex justify-between mb-4">
                 <div className="  flex gap-2 items-center ">
                   <div className="inset-0 bg-blue-500 h-2 w-2 rounded-full"></div>
                   <h1 className="font-semibold">Unsuccesful pases</h1>
                 </div>
-                <p className="font-bold text-base">25</p>
+                <p className="font-bold text-base">{UnsuccelfulPass }</p>
               </div>
               <div className="flex justify-between mb-4">
                 <div className="  flex gap-2 items-center ">
                   <div className="inset-0 bg-yellow-500 h-2 w-2 rounded-full"></div>
-                  <h1 className="font-semibold">Succelful final third passes</h1>
+                  <h1 className="font-semibold">
+                    Succelful final third passes
+                  </h1>
                 </div>
 
                 <p className="font-bold text-base">
-                  25<span>%</span>
+                  {SuccesfulFinal}<span>%</span>
                 </p>
               </div>
               <div className="flex  justify-between mb-4">
                 <div className="  flex gap-2 items-center ">
                   <div className="inset-0 bg-green-500 h-2 w-2 rounded-full"></div>
-                  <h1 className="font-semibold">Unsuccelful final third passes</h1>
+                  <h1 className="font-semibold">
+                    Unsuccelful final third passes
+                  </h1>
                 </div>
 
                 <p className="font-bold text-base">
-                  25<span>%</span>
+                  {UnsuccelfulFinal}<span>%</span>
                 </p>
               </div>
             </div>
             <div className="flex justify-center flex-col w-full items-center">
               <div className="flex  gap-8">
-                <div className="relative w-20  h-20 ">
-                  <div className="absolute inset-0 bg-gray-100 rounded-full"></div>
-                  <div className="absolute inset-0 bg-sky-500  rounded-full border-3"></div>
-                  <div className="absolute inset-0 flex items-center justify-center text-white font-bold rounded-full">
-                    <span className="text-xl text-black-main z-20 ">50%</span>
-                  </div>
-                  <div className="absolute inset-1.5 bg-white rounded-full"></div>
-                  <div className="absolute inset-10 bg-transparent rounded-full"></div>
+                <div className="flex justify-center items-center ">
+                  <h2 className="z-0 absolute mt-3 font-semibold">{HasilPass} <span>%</span></h2>
+                  <div className=" w-32 h-32 z-10"><ChartPasses /></div>
                 </div>
-                <div className="relative w-20  h-20 ">
-                  <div className="absolute inset-0 bg-gray-100 rounded-full"></div>
-                  <div className="absolute inset-0 bg-sky-500  rounded-full border-3"></div>
-                  <div className="absolute inset-0 flex items-center justify-center text-white font-bold rounded-full">
-                    <span className="text-xl text-black-main z-20 ">50%</span>
-                  </div>
-                  <div className="absolute inset-1.5 bg-white rounded-full"></div>
-                  <div className="absolute inset-10 bg-transparent rounded-full"></div>
+                <div className="flex justify-center items-center ">
+                  <h2 className="z-0 absolute mt-3 font-semibold">{HasilFinal}<span>%</span></h2>
+                  <div className=" w-32 h-32 z-10">  <ChartPosition /></div>
                 </div>
               </div>
               <div className="flex flex-col justify-center items-center">
-                <h1 className="font-bold text-xl text-[#D00D00] ">344</h1>
+                <h1 className="font-bold text-xl text-[#D00D00] ">{SuccesfulPass+UnsuccelfulPass}</h1>
                 <p className="font-semibold">Total Passes</p>
               </div>
             </div>
@@ -143,13 +213,19 @@ function MatchRekap() {
               </div>
               <div className="w-full md:max-w-[226px] ">
                 {/* range tracking */}
-                <label htmlFor="range" className="flex font-bold h-6 justify-end  text-red-500">
+                <label
+                  htmlFor="range"
+                  className="flex font-bold h-6 justify-end  text-red-500"
+                >
                   <h1>{valuetackle}</h1>
                   <h1>/15</h1>
                 </label>
                 {/* range */}
                 <div className="w-full bg-gray-200  rounded-full h-2.5  ">
-                  <div className="bg-red-600 h-2.5 rounded-full " style={{ width: `${tackle_won}%` }}></div>
+                  <div
+                    className="bg-red-600 h-2.5 rounded-full "
+                    style={{ width: `${tackle_won}%` }}
+                  ></div>
                 </div>
               </div>
             </div>
@@ -166,13 +242,19 @@ function MatchRekap() {
               </div>
               <div className="w-full md:max-w-[226px] ">
                 {/* range tracking */}
-                <label htmlFor="range" className="flex font-bold h-6 justify-end  text-red-500">
+                <label
+                  htmlFor="range"
+                  className="flex font-bold h-6 justify-end  text-red-500"
+                >
                   <h1>{valueduel}</h1>
                   <h1>/9</h1>
                 </label>
                 {/* range */}
                 <div className="w-full bg-gray-200  rounded-full h-2.5  ">
-                  <div className="bg-red-600 h-2.5 rounded-full " style={{ width: `${duel_won}%` }}></div>
+                  <div
+                    className="bg-red-600 h-2.5 rounded-full "
+                    style={{ width: `${duel_won}%` }}
+                  ></div>
                 </div>
               </div>
             </div>
@@ -189,13 +271,19 @@ function MatchRekap() {
               </div>
               <div className="w-full md:max-w-[226px] ">
                 {/* range tracking */}
-                <label htmlFor="range" className="flex font-bold h-6 justify-end  text-red-500">
+                <label
+                  htmlFor="range"
+                  className="flex font-bold h-6 justify-end  text-red-500"
+                >
                   <h1>{value50_50s}</h1>
                   <h1>/1</h1>
                 </label>
                 {/* range */}
                 <div className="w-full bg-gray-200  rounded-full h-2.5  ">
-                  <div className="bg-red-600 h-2.5 rounded-full " style={{ width: `${swon}%` }}></div>
+                  <div
+                    className="bg-red-600 h-2.5 rounded-full "
+                    style={{ width: `${swon}%` }}
+                  ></div>
                 </div>
               </div>
             </div>
@@ -212,13 +300,19 @@ function MatchRekap() {
               </div>
               <div className="w-full md:max-w-[226px] ">
                 {/* range tracking */}
-                <label htmlFor="range" className="flex font-bold h-6 justify-end  text-red-500">
+                <label
+                  htmlFor="range"
+                  className="flex font-bold h-6 justify-end  text-red-500"
+                >
                   <h1>{valueiwon}</h1>
                   <h1>/7</h1>
                 </label>
                 {/* range */}
                 <div className="w-full bg-gray-200  rounded-full h-2.5  ">
-                  <div className="bg-red-600 h-2.5 rounded-full " style={{ width: `${iwon}%` }}></div>
+                  <div
+                    className="bg-red-600 h-2.5 rounded-full "
+                    style={{ width: `${iwon}%` }}
+                  ></div>
                 </div>
               </div>
             </div>
