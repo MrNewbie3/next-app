ad9be06e1e025b1df1422a5602c13d345c6a5195
lakyulakyu <martieszhidan@gmail.com>
2023-05-02 21:07:06 +0700

    fetching data :error team card

--- components/playerData.tsx
+++ components/playerData.tsx
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 import react from "react";
 import { useEffect, useRef } from "react";
-import Chart from "chart.js/auto";
+import { Chart, ChartData, HorizontalBarController, CategoryScale, LinearScale, Tooltip } from 'chart.js';
 
 // untuk memasukkan data pada range===!!
 const max = 2;
@@ -11,33 +11,34 @@ const max2 = 3;
 const value2 = 2;
 const percent3 = (value2 / max2) * 100;
 
-// const SuccesfulPass=2
-// const UnsuccelfulPass=  10
-// const HasilPass=Math.floor((SuccesfulPass/UnsuccelfulPass)*100);
 
-// const SuccesfulFinal=4
-// const UnsuccelfulFinal=7
-// const HasilFinal= Math.floor((SuccesfulFinal/UnsuccelfulFinal)*100);
+const succesfull_passes = 2;
+const unsuccesfull_passes = 10;
+const Hasil_Passes = Math.floor(
+  (succesfull_passes / unsuccesfull_passes) * 100
+);
 
+const unsuccelfull_final_thirdpasses = 4;
+const succesfull_final_third_passes = 7;
+const Hasil_Third = Math.floor(
+  (unsuccelfull_final_thirdpasses / succesfull_final_third_passes) * 100
+);
 
-const succesfull_passes=2
-const unsuccesfull_passes=  10
-const Hasil_Passes=Math.floor((succesfull_passes/unsuccesfull_passes)*100);
+const hasil_pass =
+  unsuccesfull_passes +
+  unsuccesfull_passes +
+  succesfull_final_third_passes +
+  unsuccelfull_final_thirdpasses;
 
-const unsuccelfull_final_thirdpasses=4
-const succesfull_final_third_passes=7
-const Hasil_Third= Math.floor((unsuccelfull_final_thirdpasses/succesfull_final_third_passes)*100);
+const inside_the_box = 2;
+const outside_the_box = 10;
+const Hasil_box = Math.floor((inside_the_box / outside_the_box) * 100);
 
-const hasil_pass=(unsuccesfull_passes+unsuccesfull_passes+succesfull_final_third_passes+unsuccelfull_final_thirdpasses);
-
-const inside_the_box=2
-const outside_the_box=  10
-const Hasil_box=Math.floor((inside_the_box/outside_the_box)*100);
-
-const save_caught=4
-const save_parried=7
-const Hasil_save= Math.floor((save_caught/save_parried)*100);
-const hasil_saves=(inside_the_box+outside_the_box+save_caught+save_parried)
+const save_caught = 4;
+const save_parried = 7;
+const Hasil_save = Math.floor((save_caught / save_parried) * 100);
+const hasil_saves =
+  inside_the_box + outside_the_box + save_caught + save_parried;
 
 const Chartthirdpasses = () => {
   const chartRef = useRef<HTMLCanvasElement>(null);
@@ -51,7 +52,7 @@ const Chartthirdpasses = () => {
           data: {
             datasets: [
               {
-                data: [succesfull_passes,unsuccesfull_passes],
+                data: [succesfull_passes, unsuccesfull_passes],
                 backgroundColor: ["#EFE616", "#137403"],
                 hoverBackgroundColor: ["#EFE616", "#137403"],
                 borderWidth: 10,
@@ -81,7 +82,10 @@ const ChartPasses = () => {
           data: {
             datasets: [
               {
-                data: [unsuccelfull_final_thirdpasses,succesfull_final_third_passes ],
+                data: [
+                  unsuccelfull_final_thirdpasses,
+                  succesfull_final_third_passes,
+                ],
                 backgroundColor: ["#D00D00", "#2846AF"],
                 hoverBackgroundColor: ["#D00D00", "#2846AF"],
                 borderWidth: 10,
@@ -98,7 +102,6 @@ const ChartPasses = () => {
 
   return <canvas ref={chartRef} />;
 };
-
 
 const Chartbox = () => {
   const chartRef = useRef<HTMLCanvasElement>(null);
@@ -142,7 +145,7 @@ const Chartsave = () => {
           data: {
             datasets: [
               {
-                data: [save_caught,save_parried],
+                data: [save_caught, save_parried],
                 backgroundColor: ["#D00D00", "#2846AF"],
                 hoverBackgroundColor: ["#D00D00", "#2846AF"],
                 borderWidth: 10,
@@ -162,12 +165,26 @@ const Chartsave = () => {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 function PlayerData() {
   return (
     <>
       <div className="px-10 w-full">
         <ul className=" flex text-gray-600 font-semibold justify-around max-w-[550px] h-12  items-center bg-white drop-shadow-md rounded-lg  capitalize    ">
-          <li className="bg-[#D00D00] text-white px-4 py-1 rounded-lg">last match</li>
+          <li className="bg-[#D00D00] text-white px-4 py-1 rounded-lg">
+            last match
+          </li>
           <li>season stats</li>
           <li>history</li>
           <li>medis</li>
@@ -235,7 +252,9 @@ function PlayerData() {
             </div>
 
             <div className="max-w-4xl w-full mt-6 bg-white  rounded-lg px-6 py-4 ">
-              <div className="mb-6 text-[20px] font-semibold">Passing & Ball Position </div>
+              <div className="mb-6 text-[20px] font-semibold">
+                Passing & Ball Position{" "}
+              </div>
               <div className="flex max-md:flex-col w-full gap-20">
                 <div className="md:max-w-sm w-full">
                   <div className="flex justify-between w-full mb-4">
@@ -255,17 +274,22 @@ function PlayerData() {
                   <div className="flex justify-between mb-4">
                     <div className="relative  flex gap-2 items-center ">
                       <div className="inset-0 bg-yellow-500 h-2 w-2 rounded-full"></div>
-                      <h1 className="font-semibold">Succelful final third passes</h1>
+                      <h1 className="font-semibold">
+                        Succelful final third passes
+                      </h1>
                     </div>
 
                     <p className="font-bold text-base">
-                      {unsuccesfull_passes}<span>%</span>
+                      {unsuccesfull_passes}
+                      <span>%</span>
                     </p>
                   </div>
                   <div className="flex  justify-between mb-4">
                     <div className="relative  flex gap-2 items-center ">
                       <div className="inset-0 bg-green-500 h-2 w-2 rounded-full"></div>
-                      <h1 className="font-semibold">Unsuccelful final third passes</h1>
+                      <h1 className="font-semibold">
+                        Unsuccelful final third passes
+                      </h1>
                     </div>
 
                     <p className="font-bold text-base">
@@ -275,19 +299,29 @@ function PlayerData() {
                 </div>
                 <div className="flex justify-center flex-col w-full items-center">
                   <div className="flex  gap-8">
-                  <div className="flex justify-center items-center ">
-                  <h2 className="z-0 absolute mt-3 font-semibold">{Hasil_Passes} <span>%</span></h2>
-                  <div className=" w-32 h-32 z-10"><ChartPasses /></div>
-                </div>
-                <div className="flex justify-center items-center ">
-                  <h2 className="z-0 absolute mt-3 font-semibold">{Hasil_Third}<span>%</span></h2>
-                  <div className=" w-32 h-32 z-10">  <Chartthirdpasses /></div>
-                </div>
-                   
-
+                    <div className="flex justify-center items-center ">
+                      <h2 className="z-0 absolute mt-3 font-semibold">
+                        {Hasil_Passes} <span>%</span>
+                      </h2>
+                      <div className=" w-32 h-32 z-10">
+                        <ChartPasses />
+                      </div>
+                    </div>
+                    <div className="flex justify-center items-center ">
+                      <h2 className="z-0 absolute mt-3 font-semibold">
+                        {Hasil_Third}
+                        <span>%</span>
+                      </h2>
+                      <div className=" w-32 h-32 z-10">
+                        {" "}
+                        <Chartthirdpasses />
+                      </div>
+                    </div>
                   </div>
                   <div className="flex flex-col justify-center items-center">
-                    <h1 className="font-bold text-xl text-[#D00D00] ">{hasil_pass}</h1>
+                    <h1 className="font-bold text-xl text-[#D00D00] ">
+                      {hasil_pass}
+                    </h1>
                     <p className="font-semibold">Total Passes</p>
                   </div>
                 </div>
@@ -321,9 +355,7 @@ function PlayerData() {
                     <h1 className="font-semibold">Save cought</h1>
                   </div>
 
-                  <p className="font-bold text-base">
-                    {save_caught}
-                  </p>
+                  <p className="font-bold text-base">{save_caught}</p>
                 </div>
                 <div className="flex justify-between mb-4">
                   <div className="relative  flex gap-2 items-center ">
@@ -331,25 +363,35 @@ function PlayerData() {
                     <h1 className="font-semibold">Save paried</h1>
                   </div>
 
-                  <p className="font-bold text-base">
-                    {save_parried}
-                  </p>
+                  <p className="font-bold text-base">{save_parried}</p>
                 </div>
               </div>
 
               <div className="flex flex-col items-center lg:mt-12 sm:mt-12 md:mt-0  md:justify-center  w-full">
                 <div className="flex gap-8 ">
-                <div className="flex justify-center items-center ">
-                  <h2 className="z-0 absolute mt-3 font-semibold">{Hasil_box} <span>%</span></h2>
-                  <div className=" w-32 h-32 z-10"><Chartbox/></div>
-                </div>
-                <div className="flex justify-center items-center ">
-                  <h2 className="z-0 absolute mt-3 font-semibold">{Hasil_save}<span>%</span></h2>
-                  <div className=" w-32 h-32 z-10">  <Chartsave /></div>
-                </div>
+                  <div className="flex justify-center items-center ">
+                    <h2 className="z-0 absolute mt-3 font-semibold">
+                      {Hasil_box} <span>%</span>
+                    </h2>
+                    <div className=" w-32 h-32 z-10">
+                      <Chartbox />
+                    </div>
+                  </div>
+                  <div className="flex justify-center items-center ">
+                    <h2 className="z-0 absolute mt-3 font-semibold">
+                      {Hasil_save}
+                      <span>%</span>
+                    </h2>
+                    <div className=" w-32 h-32 z-10">
+                      {" "}
+                      <Chartsave />
+                    </div>
+                  </div>
                 </div>
                 <div className="flex flex-col justify-center items-center">
-                  <h1 className="font-bold text-xl text-[#D00D00] ">{hasil_saves}</h1>
+                  <h1 className="font-bold text-xl text-[#D00D00] ">
+                    {hasil_saves}
+                  </h1>
                   <p className="font-semibold">Total Saves</p>
                 </div>
               </div>
@@ -374,7 +416,10 @@ function PlayerData() {
                 </div>
                 <div className="w-full md:max-w-[226px] ">
                   {/* range tracking */}
-                  <label htmlFor="range" className="flex font-bold h-6 justify-end  text-red-500">
+                  <label
+                    htmlFor="range"
+                    className="flex font-bold h-6 justify-end  text-red-500"
+                  >
                     <h1>0</h1>
                   </label>
                   {/* range */}
@@ -394,13 +439,19 @@ function PlayerData() {
                 </div>
                 <div className="w-full md:max-w-[226px] ">
                   {/* range tracking */}
-                  <label htmlFor="range" className="flex font-bold h-6 justify-end  text-red-500">
+                  <label
+                    htmlFor="range"
+                    className="flex font-bold h-6 justify-end  text-red-500"
+                  >
                     <h1>1</h1>
                     <h1>/2</h1>
                   </label>
                   {/* range */}
                   <div className="w-full bg-gray-200  rounded-full h-2.5  ">
-                    <div className="bg-red-600 h-2.5 rounded-full " style={{ width: `${percent1}%` }}></div>
+                    <div
+                      className="bg-red-600 h-2.5 rounded-full "
+                      style={{ width: `${percent1}%` }}
+                    ></div>
                   </div>
                 </div>
               </div>
@@ -417,13 +468,19 @@ function PlayerData() {
                 </div>
                 <div className="w-full md:max-w-[226px] ">
                   {/* range tracking */}
-                  <label htmlFor="range" className="flex font-bold h-6 justify-end  text-red-500">
+                  <label
+                    htmlFor="range"
+                    className="flex font-bold h-6 justify-end  text-red-500"
+                  >
                     <h1>1</h1>
                     <h1>/3</h1>
                   </label>
                   {/* range */}
                   <div className="w-full bg-gray-200  rounded-full h-2.5  ">
-                    <div className="bg-red-600 h-2.5 rounded-full " style={{ width: `${percent3}%` }}></div>
+                    <div
+                      className="bg-red-600 h-2.5 rounded-full "
+                      style={{ width: `${percent3}%` }}
+                    ></div>
                   </div>
                 </div>
               </div>
@@ -440,7 +497,10 @@ function PlayerData() {
                 </div>
                 <div className="w-full md:max-w-[226px] ">
                   {/* range tracking */}
-                  <label htmlFor="range" className="flex font-bold h-6 justify-end  text-red-500">
+                  <label
+                    htmlFor="range"
+                    className="flex font-bold h-6 justify-end  text-red-500"
+                  >
                     <h1>0</h1>
                   </label>
                   {/* range */}
