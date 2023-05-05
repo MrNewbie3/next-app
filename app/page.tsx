531a0cefee8713195e2e68b6a7660d22ed983cc6
MrNewbie3 <atharafiaffandy@gmail.com>
2023-05-05 10:30:22 +0700

    dev: push trigger wa bot

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,5 @@ import { useRouter } from "next/navigation";
 export default function Home() {
   const router = useRouter();
   router.push("/new_sport");
-  return (
-    <main className="flex-col flex px-10 items-center gap-11 ">
-      <h1>Hello World</h1>
-    </main>
-  );
+  return <main className="flex-col flex px-10 items-center gap-11 "></main>;
 }
