f5ed5b7dbccbd8d3c3f30c2daf296cfae670ccda
MrNewbie3 <atharafiaffandy@gmail.com>
2023-05-06 15:13:02 +0700

    dev: fixing api

--- components/server/teamcard.tsx
+++ components/server/teamcard.tsx
@@ -31,7 +31,7 @@ async function TeamCard({ params: query }: PageProps) {
       {data.success ? "" : (data = [])}
       <div className=" w-full rounded-lg py-4 ">
         <div className=" flex  justify-between max-lg:flex-col  items-center ">
-          <h1 className="capitalize font-bold opensans text-3xl">{data.club_nickname}</h1>
+          <h1 className="capitalize font-bold opensans text-3xl">{data.data.club_nickname}</h1>
           <button className="bg-[#D00D00] max-lg:mt-4 rounded-lg  text-white px-12 py-2">Edit Club</button>
         </div>
 
@@ -40,21 +40,21 @@ async function TeamCard({ params: query }: PageProps) {
             <Image src={"/images/team_img.png"} width={"128"} height={"150"} alt="" className="object-cover rounded-2xl w-[128px] h-auto "></Image>
             <div>
               <h1 className="font-semibold mt-8 text-[#9d9d9d] text-base">sepak bola</h1>
-              <h1 className="font-bold capitalize mt-1 text-2xl text-gray-900">{data.club_shortname}</h1>
+              <h1 className="font-bold capitalize mt-1 text-2xl text-gray-900">{data.data.club_shortname}</h1>
             </div>
           </div>
           <div className="data_tim w-full uppercase grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-8 gap-6 font-semibold">
             <div>
               <label className="text-xs font-bold">nama asli</label>
-              <p className=" capitalize bg-white h-10 w-full  p-2 rounded-lg">{data.club_name}</p>
+              <p className=" capitalize bg-white h-10 w-full  p-2 rounded-lg">{data.data.club_name}</p>
             </div>
             <div>
               <label className="text-xs font-bold">tanggal berdiri</label>
-              <p className=" capitalize bg-white h-10 w-full  p-2 rounded-lg">{data.club_established}(30)</p>
+              <p className=" capitalize bg-white h-10 w-full  p-2 rounded-lg">{data.data.club_established}</p>
             </div>
             <div>
               <label className="text-xs font-bold">asal club</label>
-              <p className=" capitalize bg-white h-10 w-full  p-2 rounded-lg">{data.club_origin}</p>
+              <p className=" capitalize bg-white h-10 w-full  p-2 rounded-lg">{data.data.club_origin}</p>
             </div>
             <div>
               <label className="text-xs font-bold">jumlah pemain</label>
