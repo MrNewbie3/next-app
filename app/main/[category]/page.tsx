eb05b9466dec874e239a250d5660dda61fbd51d2
MrNewbie3 <atharafiaffandy@gmail.com>
2023-06-03 23:36:19 +0700

    feat: new layout teams

--- app/main/[category]/page.tsx
+++ app/main/[category]/page.tsx
@@ -1,8 +1,9 @@
 import { cookies } from "next/headers";
+import Image from "next/image";
 import Link from "next/link";
 import { NextRequest } from "next/server";
 import React from "react";
-import { FaPlus } from "react-icons/fa";
+import { FaEdit, FaPlus, FaTrash } from "react-icons/fa";
 
 interface PageProps {
   params: {
@@ -39,20 +40,48 @@ export default async function Page({ params }: PageProps) {
           Tambah
         </Link>
       ) : (
-        <div className="wrapper grid lg:grid-cols-5 md:grid-cols-3  ">
+        <div className="wrapper grid grid-cols-3 gap-5  ">
+          <Link href={`/main/${params.category}/new_team`} className="flex flex-col justify-center active:bg-zinc-400 active:scale-75 transition duration-50 gap-4 items-center shadow-md bg-white font-semibold py-2 w-full rounded-md ">
+            <span className="text-gray-300">
+              <FaPlus className="text-9xl" />
+            </span>
+            Tambah Team
+          </Link>
           {data.data.map((data: any) => {
             return (
-              <Link key={data.id} href={`/main/${params.category}/${data.uuid}`} className=" px-7 text-center mb-5 bg-white font-semibold py-2 w-fit rounded-md ">
-                {data.club_shortname}
-              </Link>
+              <div key={data.id} className="max-w-md mx-auto hover:translate-y-[-20px] transition-all duration-300 bg-white min-w-full rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+                <Image className="object-cover w-full h-64" width={"430"} height={"430"} src={data.club_image || "https://naurok-test2.nyc3.digitaloceanspaces.com/248846/images/897517_1666207808.jpg"} alt={data.club_name} />
+                <div className="p-6">
+                  <Link href={`/main/${params.category}/${data.uuid}`} className="hover:cursor-pointer hover:underline">
+                    <h2 className="text-2xl font-bold mb-4 uppercase">{data.club_name}</h2>
+                  </Link>
+                  <p className="text-gray-600 mb-2 capitalize">
+                    <strong>Nickname:</strong> {data.club_nickname}
+                  </p>
+                  <p className="text-gray-600 mb-2 capitalize">
+                    <strong>Short Name:</strong> {data.club_shortname}
+                  </p>
+                  <p className="text-gray-600 mb-2 capitalize">
+                    <strong>Origin:</strong> {data.club_origin}
+                  </p>
+                  <p className="text-gray-600 mb-2 capitalize">
+                    <strong>Established:</strong> {data.club_established}
+                  </p>
+                  <p className="text-gray-600 capitalize">
+                    <strong>Season:</strong> {data.start_season} - {data.end_season}
+                  </p>
+                  <div className="flex justify-end mt-4">
+                    <button className="text-blue-500 hover:text-blue-700 mr-2">
+                      <FaEdit />
+                    </button>
+                    <button className="text-red-500 hover:text-red-700">
+                      <FaTrash />
+                    </button>
+                  </div>
+                </div>
+              </div>
             );
           })}
-          <Link href={`/main/${params.category}/new_team`} className="flex pl-7 gap-4 items-center bg-white font-semibold py-2 w-[170px] rounded-md ">
-            <span className="text-[#D00D00]">
-              <FaPlus />
-            </span>
-            Tambah
-          </Link>
         </div>
         // redirect(`/${category}/${data.data.id}`)
       )}
