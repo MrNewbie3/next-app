c80084ff735f9f66ff160e4e432633a80e8654a5
MrNewbie3 <atharafiaffandy@gmail.com>
2023-07-23 21:41:24 +0700

    fix: auth token bugs

--- config/axios.js
+++ config/axios.js
@@ -2,7 +2,7 @@
 const axios = require("axios");
 const { getAuthTokenClient } = require("./cookie");
 
-const auth = getAuthTokenClient();
+const auth = localStorage.getItem("token");
 const url = process.env.NEXT_PUBLIC_URL;
 const instance = axios.create({
   baseURL: url,
